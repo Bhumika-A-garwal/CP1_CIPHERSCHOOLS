e297a1e45e2cfcdf0c76f52c185377b78d0d7dcf
Bhumika-A-garwal <bg373343@gmail.com>
2020-08-15 16:31:26 +0530

    Assignment 1

---
+++ Pyramid pattern after 180 degree rotation.cpp
@@ -0,0 +1,33 @@
+/*
+/*Write a Program that takes an integer value n as input and prints first n lines of the pyramid pattern after 180 degrees of rotation.
+Following are the first 5 rows of Pascal’s Triangle.
+
+		*
+      * * 
+    * * * 
+  * * * * 
+* * * * * 
+
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n-i;j++)
+		{
+			cout<<" "<<" ";
+		}
+		for(int k=1;k<=i;k++)
+		{
+			cout<<"*"<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
--- Simple Pyramid Pattern.cpp
+++ Simple Pyramid Pattern.cpp
@@ -14,9 +14,9 @@ using namespace std;
 
 int main()
 {
-	int number;
-	cin>>number;
-	for(int i=1;i<=number;i++)
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++)
 	{
 		for(int j=1;j<=i;j++)
 		{
